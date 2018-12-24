bc8649d42ea5e2c7cbb1d97585e2baf792fa2fc7
Dr_Matic <matic.drobez@gmail.com>
2018-12-24 14:02:37 +0100

    Creating Vue.js Components and Using Template Syntax.

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,132 +1,300 @@
 <template>
-  <div>
+  <div class="content">
+    <button class="add-to-card" @click="addToCard()">Add to Cart</button>
     <div class="top-row">
-      <div class="top part">
-        <img src="./images/head-big-eye.png" title="head"/>
-        <button class="prev-selector">&#9668;</button>
-        <button class="next-selector">&#9658;</button>
+      <div class="top part" :style="[headBorderStyle]">
+        <div class="robot-name">
+          {{selectedRobot.head.title}}
+          <span v-show="selectedRobot.head.onSale" class="sale">Sale!</span>
+        </div>
+        <img v-bind:src="selectedRobot.head.src" title="head"/>
+        <button v-on:click="selectNextHead()" class="prev-selector">&#9668;</button>
+        <button v-on:click="selectPreviousHead()" class="next-selector">&#9658;</button>
       </div>
     </div>
     <div class="middle-row">
       <div class="left part">
-        <img src="./images/arm-articulated-claw.png" title="left arm"/>
-        <button class="prev-selector">&#9650;</button>
-        <button class="next-selector">&#9660;</button>
+        <img v-bind:src="selectedRobot.leftArm.src" title="left arm"/>
+        <button v-on:click="selectNextLeftArm()" class="prev-selector">&#9650;</button>
+        <button v-on:click="selectPreviousLeftArm()" class="next-selector">&#9660;</button>
       </div>
       <div class="center part">
-        <img src="./images/torso-flexible-gauged.png" title="left arm"/>
-        <button class="prev-selector">&#9668;</button>
-        <button class="next-selector">&#9658;</button>
+        <img v-bind:src="selectedRobot.torso.src" title="left arm"/>
+        <button v-on:click="selectNextTorso()" class="prev-selector">&#9668;</button>
+        <button v-on:click="selectPrevious()" class="next-selector">&#9658;</button>
       </div>
       <div class="right part">
-        <img src="./images/arm-dual-claw.png" title="left arm"/>
-        <button class="prev-selector">&#9650;</button>
-        <button class="next-selector">&#9660;</button>
+        <img v-bind:src="selectedRobot.rightArm.src" title="left arm"/>
+        <button v-on:click="selectNextRightArm()" class="prev-selector">&#9650;</button>
+        <button v-on:click="selectPreviousRightArm()" class="next-selector">&#9660;</button>
       </div>
     </div>
     <div class="bottom-row">
       <div class="bottom part">
-        <img src="./images/base-single-wheel.png" title="left arm"/>
-        <button class="prev-selector">&#9668;</button>
-        <button class="next-selector">&#9658;</button>
+        <img v-bind:src="selectedRobot.base.src" title="left arm"/>
+        <button v-on:click="selectedNextBase()" class="prev-selector">&#9668;</button>
+        <button v-on:click="selectPreviousBase()" class="next-selector">&#9658;</button>
       </div>
+    </div>
+    <div>
+      <h1>Card</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Robot</th>
+            <th class="cost">Cost</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(robot, index) in cart" :key="index">
+              <td>{{ robot.head.title }}</td>
+              <td class="cost">{{robot.cost}}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "RobotBuilder",
+import availableParts from '../data/parts';
+import createdHookMixin from './created-hook-mixin';
+
+function getPreviousValidIndex(index, length) {
+  const deprecatedIndex = index - 1;
+  return deprecatedIndex < 0 ? length - 1 : deprecatedIndex;
+}
+
+function getNextValidIndex(index, length) {
+  const incrementedIndex = index + 1;
+  return incrementedIndex > length - 1 ? 0 : incrementedIndex;
+}
+
+export default {
+  name: 'RobotBuilder',
+  data() {
+    return {
+      availableParts,
+      cart: [],
+      selectedHeadIndex: 0,
+      selectedLeftArmIndex: 0,
+      selectedRightArmIndex: 0,
+      selectedTorsoIndex: 0,
+      selectedLegsIndex: 0,
     };
+  },
+  mixins: [createdHookMixin],
+  computed: {
+    headBorderStyle() {
+      return {
+        border: this.selectedRobot.head.onSale
+          ? '3px solid red' : '3px solid #aaa',
+      };
+    },
+    selectedRobot() {
+      return {
+        head: availableParts.heads[this.selectedHeadIndex],
+        leftArm: availableParts.arms[this.selectedLeftArmIndex],
+        rightArm: availableParts.arms[this.selectedRightArmIndex],
+        torso: availableParts.torsos[this.selectedTorsoIndex],
+        base: availableParts.bases[this.selectedLegsIndex],
+      };
+    },
+  },
+  methods: {
+    addToCard() {
+      const robot = this.selectedRobot;
+      const cost = robot.head.cost
+        + robot.leftArm.cost
+        + robot.rightArm.cost
+        + robot.torso.cost
+        + robot.base.cost;
+      this.cart.push(Object.assign({}, robot, { cost }));
+    },
+
+    selectNextHead() {
+      this.selectedHeadIndex = getNextValidIndex(this.selectedHeadIndex,
+        availableParts.heads.length);
+      this.selectedHeadIndex += 1;
+    },
+
+    selectPreviousHead() {
+      this.selectedHeadIndex = getPreviousValidIndex(this.selectedHeadIndex,
+        availableParts.heads.length);
+      this.selectedHeadIndex -= 1;
+    },
+
+    selectNextLeftArm() {
+      this.selectedLeftArmIndex = getNextValidIndex(this.selectedLeftArmIndex,
+        availableParts.arms.length);
+      this.selectedLeftArmIndex += 1;
+    },
+
+    selectPreviousLeftArm() {
+      this.selectedLeftArmIndex = getPreviousValidIndex(this.selectedLeftArmIndex,
+        availableParts.arms.length);
+      this.selectedLeftArmIndex -= 1;
+    },
+
+    selectNextRightArm() {
+      this.selectedRightArmIndex = getNextValidIndex(this.selectedRightArmIndex,
+        availableParts.arms.length);
+      this.selectedRightArmIndex += 1;
+    },
+
+    selectPreviousRightArm() {
+      this.selectedRightArmIndex = getPreviousValidIndex(this.selectedRightArmIndex,
+        availableParts.arms.length);
+      this.selectedRightArmIndex -= 1;
+    },
+
+    selectNextTorso() {
+      this.selectedTorsoIndex = getNextValidIndex(this.selectedTorsoIndex,
+        availableParts.torsos.length);
+      this.selectedTorsoIndex += 1;
+    },
+
+    selectedNextBase() {
+      this.selectedLegsIndex = getNextValidIndex(this.selectedLegsIndex,
+        availableParts.bases.length);
+      this.selectedLegsIndex += 1;
+    },
+
+    selectPrevious() {
+      this.selectedTorsoIndex = getPreviousValidIndex(this.selectedTorsoIndex,
+        availableParts.torsos.length);
+      this.selectedTorsoIndex -= 1;
+    },
+
+    selectPreviousBase() {
+      this.selectedLegsIndex = getPreviousValidIndex(this.selectedLegsIndex,
+        availableParts.bases.length);
+      this.selectedLegsIndex -= 1;
+    },
+  },
+};
 </script>
 
-<style scoped>
-  .part {
-    position: relative;
+<style lang="scss" scoped>
+.part {
+  position: relative;
+  width:165px;
+  height:165px;
+  border: 3px solid #aaa;
+}
+.part {
+  img {
     width:165px;
-    height:165px;
-    border: 3px solid #aaa;
   }
-  .part img {
-    width:165px;
-  }
-  .top-row {
-    display:flex;
-    justify-content: space-around;
-  }
-  .middle-row {
-    display:flex;
-    justify-content: center;
-  }
-  .bottom-row {
-    display:flex;
-    justify-content: space-around;
-    border-top: none;
-  }
-  .head {
-    border-bottom: none;
-  }
-  .left {
-    border-right: none;
-  }
-  .right {
-    border-left: none;
-  }
-  .left img {
-    transform: rotate(-90deg);
-  }
-  .right img {
-    transform: rotate(90deg);
-  }
-  .bottom {
-    border-top: none;
-  }
-  .prev-selector {
-    position: absolute;
-    z-index:1;
-    top: -3px;
-    left: -28px;
-    width: 25px;
-    height: 171px;
-  }
-  .next-selector {
-    position: absolute;
-    z-index:1;
-    top: -3px;
-    right: -28px;
-    width: 25px;
-    height: 171px;
-  }
-  .center .prev-selector, .center .next-selector {
-    opacity:0.8;
-  }
-  .left .prev-selector {
-    top: -28px;
-    left: -3px;
-    width: 144px;
-    height: 25px;
-  }
-  .left .next-selector {
-    top: auto;
-    bottom: -28px;
-    left: -3px;
-    width: 144px;
-    height: 25px;
-  }
-  .right .prev-selector {
-    top: -28px;
-    left: 24px;
-    width: 144px;
-    height: 25px;
-  }
-  .right .next-selector {
-    top: auto;
-    bottom: -28px;
-    left: 24px;
-    width: 144px;
-    height: 25px;
-  }
-  .right .next-selector {
-    right: -3px;
-  }
+}
+.top-row {
+  display:flex;
+  justify-content: space-around;
+}
+.middle-row {
+  display:flex;
+  justify-content: center;
+}
+.bottom-row {
+  display:flex;
+  justify-content: space-around;
+  border-top: none;
+}
+.head {
+  border-bottom: none;
+}
+.left {
+  border-right: none;
+}
+.right {
+  border-left: none;
+}
+.left img {
+  transform: rotate(-90deg);
+}
+.right img {
+  transform: rotate(90deg);
+}
+.bottom {
+  border-top: none;
+}
+.prev-selector {
+  position: absolute;
+  z-index:1;
+  top: -3px;
+  left: -28px;
+  width: 25px;
+  height: 171px;
+}
+.next-selector {
+  position: absolute;
+  z-index:1;
+  top: -3px;
+  right: -28px;
+  width: 25px;
+  height: 171px;
+}
+.center .prev-selector, .center .next-selector {
+  opacity:0.8;
+}
+.left .prev-selector {
+  top: -28px;
+  left: -3px;
+  width: 144px;
+  height: 25px;
+}
+.left .next-selector {
+  top: auto;
+  bottom: -28px;
+  left: -3px;
+  width: 144px;
+  height: 25px;
+}
+.right .prev-selector {
+  top: -28px;
+  left: 24px;
+  width: 144px;
+  height: 25px;
+}
+.right .next-selector {
+  top: auto;
+  bottom: -28px;
+  left: 24px;
+  width: 144px;
+  height: 25px;
+}
+.right .next-selector {
+  right: -3px;
+}
+
+.robot-name {
+  position: absolute;
+  top: -25px;
+  text-align: center;
+  width: 100%;
+}
+
+.sale {
+  color: red;
+}
+
+.content {
+  position: relative;
+}
+
+.add-to-card {
+  position: absolute;
+  right: 30px;
+  width: 220px;
+  padding: 3px;
+  font-size: 16px;
+}
+
+td, th {
+  text-align: left;
+  padding: 5px 20px 0 0;
+}
+.cost {
+  text-align: right;
+}
 </style>
